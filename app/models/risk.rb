5e1bd3c61b726ef25b5ea0632094aa7de601dd8c
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-05 19:45:14 -0400

    feat: add validations to Risk model and display error messages in the form

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -1,2 +1,6 @@
 class Risk < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :severity, presence: true, inclusion: { in: %w[low medium high] }
+  validates :mitigations, presence: true, length: { minimum: 10 }
 end
